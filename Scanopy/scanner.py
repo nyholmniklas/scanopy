eb0350ee6beedc7f6adce177fca446474ecf97b0
Niklas <nyholmniklas@gmail.com>
2014-04-24 23:58:44 +0300

    Refactored getIpAddressesFromRange() method

--- Scanopy/scanner.py
+++ Scanopy/scanner.py
@@ -1,3 +1,4 @@
+import struct
 from socket import *
 
 class Scanner:
@@ -26,13 +27,8 @@ class Scanner:
     def getHostByIp(self, ip):
         return gethostbyaddr(ip)[0]
             
-    def getIpAddressesFromRange(self, start_ip_address, end_ip_address):
-        start_address_elements = start_ip_address.split('.')
-        end_address_elements = end_ip_address.split('.')
-        ip_list = []
-        for a in range(int(start_address_elements[0]), int(end_address_elements[0])+1):
-            for b in range(int(start_address_elements[1]), int(end_address_elements[1])+1):
-                for c in range(int(start_address_elements[2]), int(end_address_elements[2])+1):
-                    for d in range(int(start_address_elements[3]), int(end_address_elements[3])+1):
-                        ip_list.append(str(a)+"."+str(b)+"."+str(c)+"."+str(d))
-        return ip_list
+    def getIpAddressesFromRange(self, start, end):
+        ipstruct = struct.Struct('>I')
+        start, = ipstruct.unpack(inet_aton(start))
+        end, = ipstruct.unpack(inet_aton(end))
+        return [inet_ntoa(ipstruct.pack(i)) for i in range(start, end+1)]
